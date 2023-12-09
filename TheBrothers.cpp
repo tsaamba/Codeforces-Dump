19868176710a348290d81e1cf0becbf4ea85f8dd
tsaamba <146667565+tsaamba@users.noreply.github.com>
2023-12-09 19:58:11 +0530

    TheBrothers.cpp

---
+++ TheBrothers.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+    string f1,s1;
+    cin>>f1>>s1;
+    string f2,s2;
+    cin>>f2>>s2;
+  // to Determine whether they are brothers or not
+    
+if(s1==s2){
+    
+    cout<<"ARE Brothers";
+}
+    else{
+        
+        cout<<"NOT";
+    }
+    return 0;
+}
